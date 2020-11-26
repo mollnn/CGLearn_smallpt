8e2f69bdffcd4d1b68aaba34b5c7f59a686daea4
mollnn <1084918073@qq.com>
2020-11-26 16:50:34 +0800

    用 xorshf96 作为随机数发生器，修改了变量名

--- smallpt.cpp
+++ smallpt.cpp
@@ -1,13 +1,68 @@
 #include <bits/stdc++.h>
+#include<windows.h>
+#pragma comment( lib,"winmm.lib" )
+
 using namespace std;
-#define pi 3.1415926535897932384626433832795 // *** Added for VS2012
-double erand48(unsigned short seed[3])
+#define pi 3.1415926535897932384626433832795
+
+
+struct Timer
 {
-    return (double)rand() / (double)RAND_MAX;
-} // *** Added for VS2012
+    LARGE_INTEGER t1, t2, tc;
+    void Start()//开始计时
+    {
+        QueryPerformanceFrequency(&tc);
+        QueryPerformanceCounter(&t1);
+    }
+    void End()//结束计时
+    {
+        QueryPerformanceCounter(&t2);
+    }
+    Timer()
+    {
+        Start();
+    }
+    double GetTime()// 获取计时结果
+    {
+        double ans = (t2.QuadPart - t1.QuadPart) * 1.0 / tc.QuadPart;
+        return ans;
+    }
+    void Print()// 直接打印计时结果
+    {
+        double ans = (t2.QuadPart - t1.QuadPart) * 1.0 / tc.QuadPart;
+        printf("%.6f", ans);
+    }
+};
+
+int STDRANDOM()
+{
+    return rand()*rand();
+}
+
+unsigned int xorshf96()
+{
+    static unsigned int x = STDRANDOM(), y = STDRANDOM(), z = STDRANDOM();
+    unsigned int t;
+    x ^= x << 16;
+    x ^= x >> 5;
+    x ^= x << 1;
+    t = x;
+    x = y;
+    y = z;
+    z = t ^ x ^ y;
+    return z;
+}
+
+// 随机数生成器
+double RAND(unsigned short seed[3])
+{
+    return (double)xorshf96() / (double)0xffffffffU;
+}
+
+// 3 维向量，用于空间向量和颜色的表示
 struct Vec
-{                   // Usage: time ./smallpt 5000 && xv image.ppm
-    double x, y, z; // position, also color (r,g,b)
+{
+    double x, y, z;
     Vec(double x_ = 0, double y_ = 0, double z_ = 0)
     {
         x = x_;
@@ -17,32 +72,32 @@ struct Vec
     Vec operator+(const Vec &b) const { return Vec(x + b.x, y + b.y, z + b.z); }
     Vec operator-(const Vec &b) const { return Vec(x - b.x, y - b.y, z - b.z); }
     Vec operator*(double b) const { return Vec(x * b, y * b, z * b); }
-    Vec mult(const Vec &b) const { return Vec(x * b.x, y * b.y, z * b.z); }
-    Vec &norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
-    double dot(const Vec &b) const { return x * b.x + y * b.y + z * b.z; } // cross:
+    Vec DirectMult(const Vec &b) const { return Vec(x * b.x, y * b.y, z * b.z); }
+    Vec &Normal() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
+    double Dot(const Vec &b) const { return x * b.x + y * b.y + z * b.z; } // cross:
     Vec operator%(Vec &b) { return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
 };
 struct Ray
 {
-    Vec o, d;
-    Ray(Vec o_, Vec d_) : o(o_), d(d_) {}
+    Vec origin, direction;
+    Ray(Vec o_, Vec d_) : origin(o_), direction(d_) {}
 };
 enum Refl_t
 {
-    DIFF,
-    SPEC,
-    REFR
+    DIFFUSE,
+    SPECULAR,
+    REFRECT
 }; // material types, used in radiance()
 struct Sphere
 {
-    double rad;  // radius
-    Vec p, e, c; // position, emission, color
-    Refl_t refl; // reflection type (DIFFuse, SPECular, REFRactive)
-    Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_) : rad(rad_), p(p_), e(e_), c(c_), refl(refl_) {}
-    double intersect(const Ray &r) const
-    {                     // returns distance, 0 if nohit
-        Vec op = p - r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-        double t, eps = 1e-4, b = op.dot(r.d), det = b * b - op.dot(op) + rad * rad;
+    double radius; // radius
+    Vec p, e, c;   // position, emission, color
+    Refl_t refl;   // reflection type (DIFFuse, SPECular, REFRactive)
+    Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_) : radius(rad_), p(p_), e(e_), c(c_), refl(refl_) {}
+    double intersect(const Ray &ray) const
+    {                            // returns distance, 0 if nohit
+        Vec op = p - ray.origin; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+        double t, eps = 1e-4, b = op.Dot(ray.direction), det = b * b - op.Dot(op) + radius * radius;
         if (det < 0)
             return 0;
         else
@@ -52,96 +107,131 @@ struct Sphere
 };
 Sphere spheres[] = {
     //Scene: radius, position, emission, color, material
-    Sphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25), DIFF),   //Left
-    Sphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75), DIFF), //Rght
-    Sphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF),         //Back
-    Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF),               //Frnt
-    Sphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF),         //Botm
-    Sphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75), DIFF), //Top
-    Sphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC),        //Mirr
-    Sphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFR),        //Glas
-    Sphere(600, Vec(50, 681.6 - .27, 81.6), Vec(12, 12, 12), Vec(), DIFF)     //Lite
+    Sphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25), DIFFUSE),   //Left
+    Sphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75), DIFFUSE), //Rght
+    Sphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFFUSE),         //Back
+    Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFFUSE),               //Frnt
+    Sphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFFUSE),         //Botm
+    Sphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75), DIFFUSE), //Top
+    Sphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPECULAR),       //Mirr
+    Sphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFRECT),        //Glas
+    Sphere(600, Vec(50, 681.6 - .27, 21.6), Vec(12, 12, 12), Vec(), DIFFUSE)     //Lite
 };
-inline double clamp(double x) { return x < 0 ? 0 : x > 1 ? 1 : x; }
-inline int toInt(double x) { return int(pow(clamp(x), 1 / 2.2) * 255 + .5); }
-inline bool intersect(const Ray &r, double &t, int &id)
+inline double ClampValue(double x) { return x < 0 ? 0 : x > 1 ? 1 : x; }
+inline int ColorFloat2Int(double x) { return int(pow(ClampValue(x), 1 / 2.2) * 255 + .5); }
+inline bool GetIntersect(const Ray &ray, double &t, int &id)
 {
     double n = sizeof(spheres) / sizeof(Sphere), d, inf = t = 1e20;
     for (int i = int(n); i--;)
-        if ((d = spheres[i].intersect(r)) && d < t)
+        if ((d = spheres[i].intersect(ray)) && d < t)
         {
             t = d;
             id = i;
         }
     return t < inf;
 }
-Vec radiance(const Ray &r, int depth, unsigned short *Xi)
+Vec Radiance(const Ray &ray, int depth, unsigned short *nRandSeeds)
 {
-    double t;   // distance to intersection
-    int id = 0; // id of intersected object
-    if (!intersect(r, t, id))
-        return Vec();                // if miss, return black
-    const Sphere &obj = spheres[id]; // the hit object
-    Vec x = r.o + r.d * t, n = (x - obj.p).norm(), nl = n.dot(r.d) < 0 ? n : n * -1, f = obj.c;
-    double p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y : f.z; // max refl
+    double dist; // distance to intersection
+    int id = 0;  // id of intersected object
+    if (!GetIntersect(ray, dist, id))
+        return Vec();                                                           // if miss, return black
+    const Sphere &obj = spheres[id];                                            // the hit object
+    Vec x = ray.origin + ray.direction * dist;                                  // intersection point
+    Vec normalOut = (x - obj.p).Normal();                                       // out-normal of sphere
+    Vec normal = normalOut.Dot(ray.direction) < 0 ? normalOut : normalOut * -1; // fixed surface normal
+    Vec objColor = obj.c;                                                       // object color
+    double p = objColor.x > objColor.y && objColor.x > objColor.z
+                   ? objColor.x
+                   : objColor.y > objColor.z
+                         ? objColor.y
+                         : objColor.z; // max reflection
     if (depth > 100)
-        return obj.e; // *** Added to prevent stack overflow
+        return obj.e; //  prevent stack overflow
     if (++depth > 5)
-        if (erand48(Xi) < p)
-            f = f * (1 / p);
+        if (RAND(nRandSeeds) < p)
+            objColor = objColor * (1 / p);
         else
-            return obj.e; //R.R.
-    if (obj.refl == DIFF)
-    { // Ideal DIFFUSE reflection
-        double r1 = 2 * pi * erand48(Xi), r2 = erand48(Xi), r2s = sqrt(r2);
-        Vec w = nl, u = ((fabs(w.x) > .1 ? Vec(0, 1) : Vec(1)) % w).norm(), v = w % u;
-        Vec d = (u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).norm();
-        return obj.e + f.mult(radiance(Ray(x, d), depth, Xi));
+            return obj.e;    //R.R.
+    if (obj.refl == DIFFUSE) // Ideal DIFFUSE reflection
+    {
+        double r1 = 2 * pi * RAND(nRandSeeds);
+        double r2 = RAND(nRandSeeds);
+        double r2sqrt = sqrt(r2);
+        Vec w = normal;
+        Vec u = ((fabs(w.x) > .1 ? Vec(0, 1) : Vec(1)) % w).Normal();
+        Vec v = w % u;
+        Vec d = (u * cos(r1) * r2sqrt + v * sin(r1) * r2sqrt + w * sqrt(1 - r2)).Normal();
+        return obj.e + objColor.DirectMult(Radiance(Ray(x, d), depth, nRandSeeds));
     }
-    else if (obj.refl == SPEC) // Ideal SPECULAR reflection
-        return obj.e + f.mult(radiance(Ray(x, r.d - n * 2 * n.dot(r.d)), depth, Xi));
-    Ray reflRay(x, r.d - n * 2 * n.dot(r.d)); // Ideal dielectric REFRACTION
-    bool into = n.dot(nl) > 0;                // Ray from outside going in?
-    double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = r.d.dot(nl), cos2t;
-    if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0) // Total internal reflection
-        return obj.e + f.mult(radiance(reflRay, depth, Xi));
-    Vec tdir = (r.d * nnt - n * ((into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)))).norm();
-    double a = nt - nc, b = nt + nc, R0 = a * a / (b * b), c = 1 - (into ? -ddn : tdir.dot(n));
-    double Re = R0 + (1 - R0) * c * c * c * c * c, Tr = 1 - Re, P = .25 + .5 * Re, RP = Re / P, TP = Tr / (1 - P);
-    // Russian roulette
-    return obj.e + f.mult(depth > 2 ? (erand48(Xi) < P ? radiance(reflRay, depth, Xi) * RP
-                                                       : radiance(Ray(x, tdir), depth, Xi) * TP)
-                                    : radiance(reflRay, depth, Xi) * Re + radiance(Ray(x, tdir), depth, Xi) * Tr);
+    else if (obj.refl == SPECULAR) // Ideal SPECULAR reflection
+    {
+        return obj.e + objColor.DirectMult(Radiance(Ray(x, ray.direction - normalOut * 2 * normalOut.Dot(ray.direction)), depth, nRandSeeds));
+    }
+    else
+    {
+        Ray reflRay(x, ray.direction - normalOut * 2 * normalOut.Dot(ray.direction)); // Ideal dielectric REFRACTION
+        bool into = normalOut.Dot(normal) > 0;                                        // Ray from outside going in?
+        double nc = 1;
+        double nt = 1.5;
+        double nnt = into ? nc / nt : nt / nc;
+        double ddn = ray.direction.Dot(normal);
+        double cos2t;
+        if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0) // Total internal reflection
+        {
+            return obj.e + objColor.DirectMult(Radiance(reflRay, depth, nRandSeeds));
+        }
+        else
+        {
+            Vec tdir = (ray.direction * nnt - normalOut * ((into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)))).Normal();
+            double a = nt - nc;
+            double b = nt + nc;
+            double R0 = a * a / (b * b);
+            double c = 1 - (into ? -ddn : tdir.Dot(normalOut));
+            double Re = R0 + (1 - R0) * c * c * c * c * c;
+            double Tr = 1 - Re;
+            double P = .25 + .5 * Re;
+            double RP = Re / P;
+            double TP = Tr / (1 - P);
+            // Russian roulette
+            return obj.e + objColor.DirectMult(depth > 2 ? (RAND(nRandSeeds) < P ? Radiance(reflRay, depth, nRandSeeds) * RP
+                                                                                 : Radiance(Ray(x, tdir), depth, nRandSeeds) * TP)
+                                                         : Radiance(reflRay, depth, nRandSeeds) * Re + Radiance(Ray(x, tdir), depth, nRandSeeds) * Tr);
+        }
+    }
 }
 int main(int argc, char *argv[])
 {
-    int w = 256, h = 192, samps = 2 /*argc==2 ? atoi(argv[1])/4 : 1*/; // # samples
-    Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm());            // cam pos, dir
-    Vec cx = Vec(w * .5135 / h), cy = (cx % cam.d).norm() * .5135, r, *c = new Vec[w * h];
-#pragma omp parallel for schedule(dynamic, 1) private(r) // OpenMP
+    srand(time(NULL));
+
+    Timer timer;
+    int w = 256, h = 192, samps = 8; // # samples
+    Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).Normal());        // cam pos, dir
+    Vec cx = Vec(w * .5135 / h), cy = (cx % cam.direction).Normal() * .5135, ray, *c = new Vec[w * h];
+#pragma omp parallel for schedule(dynamic, 1) private(ray) // OpenMP
     for (int y = 0; y < h; y++)
     { // Loop over image rows
         // *** Commented out for Visual Studio, fprintf is not thread-safe
         //fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*4,100.*y/(h-1));
-        unsigned short Xi[3] = {0, 0, y * y * y};                   // *** Moved outside for VS2012
+        unsigned short nRandSeeds[3] = {0, 0, y * y * y};           // *** Moved outside for VS2012
         for (unsigned short x = 0; x < w; x++)                      // Loop cols
             for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++) // 2x2 subpixel rows
-                for (int sx = 0; sx < 2; sx++, r = Vec())
+                for (int sx = 0; sx < 2; sx++, ray = Vec())
                 { // 2x2 subpixel cols
                     for (int s = 0; s < samps; s++)
                     {
-                        double r1 = 2 * erand48(Xi), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-                        double r2 = 2 * erand48(Xi), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
+                        double r1 = 2 * RAND(nRandSeeds), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
+                        double r2 = 2 * RAND(nRandSeeds), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
                         Vec d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
-                                cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.d;
-                        r = r + radiance(Ray(cam.o + d * 140, d.norm()), 0, Xi) * (1. / samps);
+                                cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.direction;
+                        ray = ray + Radiance(Ray(cam.origin + d * 140, d.Normal()), 0, nRandSeeds) * (1. / samps);
                     } // Camera rays are pushed ^^^^^ forward to start in interior
-                    c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
+                    c[i] = c[i] + Vec(ClampValue(ray.x), ClampValue(ray.y), ClampValue(ray.z)) * .25;
                 }
-        cout << y << "/" << h << endl;
+        cout << "Rendering Progress: " << setiosflags(ios::fixed)<<setprecision(2)<<(double)((y+1)*100.0/h) <<"%"<< endl;
     }
     FILE *f = fopen("image.ppm", "w"); // Write image to PPM file.
     fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
     for (int i = 0; i < w * h; i++)
-        fprintf(f, "%d\n%d\n%d\n", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
+        fprintf(f, "%d\n%d\n%d\n", ColorFloat2Int(c[i].x), ColorFloat2Int(c[i].y), ColorFloat2Int(c[i].z));
 }
